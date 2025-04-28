a6a9f43bef37ddadf56c972106db6fe2b97f7e7b
JaesungGo <102339979+JaesungGo@users.noreply.github.com>
2025-04-28 19:02:15 +0900

    Feat: 결제 상태 조회 API 구현

--- src/main/java/me/jaesung/simplepg/domain/vo/payment/PaymentStatus.java
+++ src/main/java/me/jaesung/simplepg/domain/vo/payment/PaymentStatus.java
@@ -1,9 +1,19 @@
 package me.jaesung.simplepg.domain.vo.payment;
 
 public enum PaymentStatus {
-    READY,   //결제 요청 생성
-    APPROVED,  // 결제 요청 승인
-    COMPLETED,  // 결제 최종 완료
-    FAILED, // 결제 실패
-    CANCELLED // 결제 취소
+    READY("아직 결제가 완료되지 않았습니다."),
+    APPROVED("결제가 승인되었습니다."),
+    COMPLETED("결제가 완료되었습니다."),
+    CANCELED("결제가 취소되었습니다."),
+    FAILED("결제가 실패했습니다.");
+
+    private final String message;
+
+    PaymentStatus(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
 }
--- src/main/java/me/jaesung/simplepg/mapper/PaymentMapper.java
+++ src/main/java/me/jaesung/simplepg/mapper/PaymentMapper.java
@@ -3,8 +3,12 @@ package me.jaesung.simplepg.mapper;
 import me.jaesung.simplepg.domain.dto.payment.PaymentDTO;
 import me.jaesung.simplepg.domain.vo.payment.Payment;
 
+import java.util.Optional;
+
 public interface PaymentMapper {
     void insertPayment(PaymentDTO paymentDTO);
 
     boolean existsByOrderNo(String orderNo);
+
+    Optional<PaymentDTO> findByPaymentKey(String paymentKey);
 }
