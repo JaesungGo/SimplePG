a70da7ed781d27c7912e3063291967db8ca1867c
JaesungGo <102339979+JaesungGo@users.noreply.github.com>
2025-06-16 23:54:44 +0900

    Feat: 비동기 관련 설정 클래스

--- src/main/java/me/jaesung/simplepg/config/WebConfig.java
+++ src/main/java/me/jaesung/simplepg/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[]{AppConfig.class, SecurityConfig.class};
+        return new Class[]{AppConfig.class, SecurityConfig.class, AsyncConfig.class};
     }
 
     @Override
